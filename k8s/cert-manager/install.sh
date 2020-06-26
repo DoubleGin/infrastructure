593d25abc03b26911ad143a1301365395eaa2a8d
Rob Salmond <rob@salmond.ca>
2020-06-25 22:04:20 -0600

    argo prep for cert-manager

--- k8s/cert-manager/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
