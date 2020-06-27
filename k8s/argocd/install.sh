db75c0f0e9da72e6d795e559ec9369a0b957bebb
Rob Salmond <rob@salmond.ca>
2020-06-26 22:23:22 -0600

    reorg

--- k8s/argocd/install.sh
+++ k8s/argocd/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 kubectl create ns argocd
+
 # https://raw.githubusercontent.com/argoproj/argo-cd/v1.6.1/manifests/install.yaml
 kubectl apply -n argocd -f install.yaml
 
 # give myself cluster admin role
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value account)"
+
+# apps
+
+kubectl apply -f applications/*.yaml
