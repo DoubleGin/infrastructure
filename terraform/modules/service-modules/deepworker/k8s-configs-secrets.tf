7e4e6e55aa2d49796de2c7117b9ed70876089107
Rob Salmond <rob@salmond.ca>
2020-07-03 21:17:50 -0600

    simplify

--- terraform/modules/service-modules/deepworker/k8s-configs-secrets.tf
+++ terraform/modules/service-modules/deepworker/k8s-configs-secrets.tf
@@ -5,15 +5,16 @@ resource "kubernetes_config_map" "deepworker" {
   }
 
   data = {
-    DEEPWORKER_REDIS_HOST                             = var.redis_service["host"]
-    DEEPWORKER_REDIS_PORT                             = var.redis_service["port"]
-    DEEPWORKER_REDIS_DB_ID                            = "0"
-    DEEPWORKER_PODSCRIBER_WEB_HOST                    = "podscriber-web.podscriber.svc.cluster.local"
-    DEEPWORKER_DEEPSPEECH_TRANSCRIPT_MEDIA_GCS_BUCKET = google_storage_bucket.deepspeech_transcripts.name
-    #DEEPWORKER_PODCAST_MEDIA_GCS_BUCKET    = google_storage_bucket.podcasts.name
-    #DEEPWORKER_EXCERPT_MEDIA_GCS_BUCKET    = google_storage_bucket.excerpts.name
-    #DEEPWORKER_TRANSCRIPT_MEDIA_GCS_BUCKET = google_storage_bucket.transcripts.name
+    WORKER_REDIS_HOST                             = var.redis_service["host"]
+    WORKER_REDIS_PORT                             = var.redis_service["port"]
+    WORKER_REDIS_DB_ID                            = "0"
+    WORKER_PODSCRIBER_WEB_HOST                    = "podscriber-web.podscriber.svc.cluster.local"
+    WORKER_DEEPSPEECH_TRANSCRIPT_MEDIA_GCS_BUCKET = google_storage_bucket.deepspeech_transcripts.name
+    WORKER_DEEPSPEECH_VERSION                     = "0.7.4"
+    #WORKER_PODCAST_MEDIA_GCS_BUCKET    = google_storage_bucket.podcasts.name
+    #WORKER_EXCERPT_MEDIA_GCS_BUCKET    = google_storage_bucket.excerpts.name
+    #WORKER_TRANSCRIPT_MEDIA_GCS_BUCKET = google_storage_bucket.transcripts.name
     # settings module will fetch credentials blob from this google secret manager secret at startup
-    #DEEPWORKER_SECRET_MANAGER_SECRET_NAME = "deepworker"
+    #WORKER_SECRET_MANAGER_SECRET_NAME = "deepworker"
   }
 }
