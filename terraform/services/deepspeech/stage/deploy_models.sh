6eeb78d4ff2ec39b2d9755c8f50e6852ed5198b1
Rob Salmond <rob@salmond.ca>
2020-07-04 21:31:01 -0600

    fix model bucket data

--- terraform/services/deepspeech/stage/deploy_models.sh
+++ terraform/services/deepspeech/stage/deploy_models.sh
@@ -5,5 +5,5 @@
 wget -nc https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.pbmm
 wget -nc https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.scorer
 
-gsutil -m cp deepspeech-0.7.4-models.pbmm gs://oddmark-deepspeech-transcripts-stage/
-gsutil -m cp deepspeech-0.7.4-models.scorer gs://oddmark-deepspeech-transcripts-stage/
+gsutil -m cp deepspeech-0.7.4-models.pbmm gs://oddmark-deepspeech-models-stage/
+gsutil -m cp deepspeech-0.7.4-models.scorer gs://oddmark-deepspeech-models-stage/
