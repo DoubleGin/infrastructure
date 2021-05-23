d35bb28f8b73e3a70fed9a6e35bd5e4ed1efb027
Rob Salmond <rob@salmond.ca>
2021-05-23 17:08:07 -0400

    upgrade argo

--- k8s/argocd/install.sh
+++ k8s/argocd/install.sh
@@ -2,7 +2,7 @@
 
 kubectl create ns argocd
 
-# https://raw.githubusercontent.com/argoproj/argo-cd/v1.6.1/manifests/install.yaml
+# https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
 kubectl apply -n argocd -f install.yaml
 
 # give myself cluster admin role
@@ -10,3 +10,6 @@ kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-ad
 
 # apps
 find ./applications -name \*.yaml -exec kubectl apply -f {} \;
+
+# TODO:
+echo "After logging in run: `argocd repo add https://charts.bitnami.com/bitnami --type=helm --name bitnami`
